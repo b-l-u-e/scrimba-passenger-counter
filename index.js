5f8b3426bea0ff61759ccf3f7a2ad56910d20318
b-l-u-e <winnie.gitau282@gmail.com>
2024-05-07 12:21:02 +0300

    updated incrementBtn and saveBtn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+// create countEl variable
+let countEl = document.getElementById("count-el");
+//  create saveBtn variable
+let saveBtn = document.getElementById("save-btn");
+// create incrementBtn variable
+let incrementBtn = document.getElementById("increment-btn");
+// create saveEl variable
+let saveEl = document.getElementById("save-el");
 let count = 0
 
-function increment() {
+// create event listeners for incrementBtn and saveBtn
+incrementBtn.addEventListener("click", () => {
     count += 1
     countEl.textContent = count
-}
 
-function save() {
+})
+
+saveBtn.addEventListener("click", () => {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-}
+})
+
